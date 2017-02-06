f787d58bf40b0e4e6ac2c0bfb158af2c469e6f2e
Matteo Gabriele <matteo@blocklevel.nl>
2017-02-06 23:58:57 +0100

    refactor(blue location files): add the check for a blue project

--- packages/bcli/src/store.js
+++ packages/bcli/src/store.js
@@ -15,7 +15,7 @@ module.exports = co.wrap(function * (options) {
   spinner.text = 'Create a new Vuex store module'
   spinner.start()
 
-  const isBlue = options.location === 'blue' // utils.hasAppConfig()
+  const isBlue = utils.hasAppConfig()
   const name = _.kebabCase(options.name)
   const blueStructure = `${paths.appRoot}/store/modules/${name}`
   const currentFolder = `${paths.appDirectory}/${name}`
--- packages/blue-templates/src/component/index.vue
+++ packages/blue-templates/src/component/index.vue
@@ -4,6 +4,6 @@
 
 <template>
   <div :class="$style.{{ type }}">
-
+    {{ name }} {{ type }}
   </div>
 </template>
