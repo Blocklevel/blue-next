ec0b3e327221e97073cdb4507bf85392ba513beb
Matteo Gabriele <matteo@blocklevel.nl>
2017-02-07 09:32:04 +0100

    refactor(home): add landing page

--- packages/blue-templates/src/blue/src/app/page/home/home.vue
+++ packages/blue-templates/src/blue/src/app/page/home/home.vue
@@ -2,9 +2,9 @@
 <style module src="./home.css"></style>
 
 <template>
-  <div>
-    <div>
-      <h1>Blue</h1>
+  <div :class="$style.page">
+    <div :class="$style.content">
+      <h1 :class="$style.title">Blue</h1>
     </div>
   </div>
 </template>
