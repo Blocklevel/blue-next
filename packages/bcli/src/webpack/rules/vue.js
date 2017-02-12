abebd3dcb0e4069a3224b44a2544dfce5d337588
Matteo Gabriele <matteo@blocklevel.nl>
2017-02-12 13:54:47 +0100

    refactor(vue-loader): add modular composition

--- packages/bcli/src/webpack/rules/vue.js
+++ packages/bcli/src/webpack/rules/vue.js
@@ -1,8 +1,9 @@
 const combineLoaders = require('webpack-combine-loaders')
 const paths = require('../../commons/paths')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const _ = require('lodash')
 
-module.exports = {
+const vue = {
   test: /\.vue$/,
   loader: 'vue-loader',
   options: {
@@ -17,71 +18,83 @@ module.exports = {
         require('./babel'),
         require('./eslint')
       ])
-    },
-    cssModules: {
-      importLoaders: 1,
-      modules: true,
-      localIdentName: '[name]__[local]__[hash:base64:5]',
-      camelCase: true
-    },
-    postcss: function () {
-      return [
-        /**
-         * PostCSS plugin for Sass-like variables, conditionals, and iteratives
-         * https://github.com/jonathantneal/postcss-advanced-variables
-         */
-        require('postcss-advanced-variables')({
-          variables: require(`${paths.appStyle}/config/variables.js`)
-        }),
-
-        /**
-         * Plugin to inline @import rules content
-         */
-        require('postcss-import'),
-
-        /**
-         * A little bag of CSS superpowers
-         * http://simplaio.github.io/rucksack/docs
-         */
-        require('rucksack-css')({
-          // This is handled in postcss-short
-          shorthandPosition: false
-        }),
-
-        /**
-         * Unwrap nested styles like Sass does.
-         * https://github.com/postcss/postcss-nested
-         */
-        require('postcss-nested'),
-
-        /**
-         * Mixins
-         * https://github.com/postcss/postcss-mixins
-         */
-        require('postcss-mixins')({
-          mixinsDir: `${paths.appStyle}/mixin`
-        }),
-
-        /**
-         * Advanced shorthand properties
-         * https://github.com/jonathantneal/postcss-short
-         */
-        require('postcss-short'),
-
-        /**
-         * Transform W3C CSS Custom Media Queries to more compatible CSS
-         * https://github.com/postcss/postcss-custom-media
-         */
-        require('postcss-custom-media'),
-
-        /**
-         * PostCSS plugin to use tomorrow's CSS syntax
-         * https://github.com/MoOx/postcss-cssnext
-         */
-        require('postcss-cssnext')({
-          browsers: ['last 3 versions', 'iOS >= 8']
-        })
-      ]
     }
   }
 }
+
+const postcss = [
+  /**
+   * PostCSS plugin for Sass-like variables, conditionals, and iteratives
+   * https://github.com/jonathantneal/postcss-advanced-variables
+   */
+  require('postcss-advanced-variables')({
+    variables: require(`${paths.appStyle}/config/variables.js`)
+  }),
+
+  /**
+   * Plugin to inline @import rules content
+   */
+  require('postcss-import'),
+
+  /**
+   * A little bag of CSS superpowers
+   * http://simplaio.github.io/rucksack/docs
+   */
+  require('rucksack-css')({
+    // This is handled in postcss-short
+    shorthandPosition: false
+  }),
+
+  /**
+   * Unwrap nested styles like Sass does.
+   * https://github.com/postcss/postcss-nested
+   */
+  require('postcss-nested'),
+
+  /**
+   * Mixins
+   * https://github.com/postcss/postcss-mixins
+   */
+  require('postcss-mixins')({
+    mixinsDir: `${paths.appStyle}/mixin`
+  }),
+
+  /**
+   * Advanced shorthand properties
+   * https://github.com/jonathantneal/postcss-short
+   */
+  require('postcss-short'),
+
+  /**
+   * Transform W3C CSS Custom Media Queries to more compatible CSS
+   * https://github.com/postcss/postcss-custom-media
+   */
+  require('postcss-custom-media'),
+
+  /**
+   * PostCSS plugin to use tomorrow's CSS syntax
+   * https://github.com/MoOx/postcss-cssnext
+   */
+  require('postcss-cssnext')({
+    browsers: ['last 3 versions', 'iOS >= 8']
+  })
+]
+
+const cssModules = {
+  importLoaders: 1,
+  modules: true,
+  localIdentName: '[name]__[local]__[hash:base64:5]',
+  camelCase: true
+}
+
+/**
+ * @todo: Probably we need to switch the all webpack setup to a promise chain
+ * so that we can pass parameters from the bli.config file
+ * For now it only returns the CSS-modules and PostCSS setup merged with vue-loader setup
+ * but we need to add the Sass options.
+ * Bcli.config already sets the cssPreprocess value if it's not default
+ * see https://github.com/Blocklevel/blue-next/commit/54913f8bfbd2f4a3fa41fa5b53fb030b7ab1c6bf
+ */
+vue.options = Object.assign({}, vue.options, { cssModules, postcss })
+
+module.exports = vue
