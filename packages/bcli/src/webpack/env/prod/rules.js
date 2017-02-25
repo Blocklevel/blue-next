ff42f824a163b49a8038029e0edb4655234b7adc
Matteo Gabriele <matteo@blocklevel.nl>
2017-02-25 15:19:48 +0100

    chore(build):  change css class name to hash for production
    see #2

--- packages/bcli/src/webpack/env/prod/rules.js
+++ packages/bcli/src/webpack/env/prod/rules.js
@@ -1,6 +1,8 @@
 const vue = require('../../rules/vue')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
+vue.options.cssModules.localIdentName = '[hash:8]'
+
 vue.options.loaders.css = ExtractTextPlugin.extract({
   use: 'css-loader',
   fallback: 'vue-style-loader'
