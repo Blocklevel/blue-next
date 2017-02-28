6b2a17c1ebb94a9a75b26371dbc7b2ffb8ff7d1e
Matteo Gabriele <matteo@blocklevel.nl>
2017-02-28 16:27:50 +0100

    chore(rules): add limit 1 to files-loader

--- packages/bcli/src/webpack/rules/files.js
+++ packages/bcli/src/webpack/rules/files.js
@@ -2,6 +2,7 @@ module.exports = {
   test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
   loader: 'file-loader',
   options: {
+    limit: 1,
     name: 'static/[name].[ext]'
   }
 }
