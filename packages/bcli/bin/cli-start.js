28fa466b543b23ba57909fda1c24c4898cd5d9af
Matteo Gabriele <matteo@blocklevel.nl>
2017-03-02 22:26:03 +0100

    chore(webpack): change environment var from dev to development
    better consistency across the application and variables

--- packages/bcli/bin/cli-start.js
+++ packages/bcli/bin/cli-start.js
@@ -1,3 +1,3 @@
 'use strict'
-process.env.NODE_ENV = 'dev'
+process.env.NODE_ENV = 'development'
 module.exports = require('../src/server')
--- packages/bcli/src/webpack/env/development/plugins.js
+++ packages/bcli/src/webpack/env/development/plugins.js
@@ -3,12 +3,6 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = [
-  new webpack.DefinePlugin({
-    __DEV__: true,
-    'process.env': {
-      NODE_ENV: '"dev"'
-    }
-  }),
   new HtmlWebpackPlugin({
     filename: 'index.html',
     template: 'index.html',
