c5d76121e351735eba36e4519fb0fb72edce4f4d
Matteo Gabriele <matteo@blocklevel.nl>
2017-03-06 20:35:19 +0100

    fix(dev server): 0.0.0.0 not resolved
    closes #25

--- packages/blue-scripts/src/commons/utils.js
+++ packages/blue-scripts/src/commons/utils.js
@@ -2,6 +2,36 @@ const _ = require('lodash')
 const fs = require('fs')
 
 /**
+ * Checks if the platform is windows
+ * @return {Boolean}
+ */
+const isWindows = function () {
+  return !!(process.platform === 'win32')
+}
+
+/**
+ * Returns an object with the host as value and a display name
+ * This is mostly due to windows machines that don't map 0.0.0.0
+ * by default
+ * see https://github.com/Blocklevel/blue-next/issues/25
+ * @param  {String} host
+ * @return {Object}
+ */
+const getHost = function (host) {
+  if (host === '0.0.0.0' && isWindows()) {
+    return {
+      display: 'localhost',
+      value: host
+    }
+  }
+
+  return {
+    display: host,
+    value: host
+  }
+}
+
+/**
  * Require a list of files from a folder
  * @param  {String} folder
  * @return {Array<Object>}
@@ -14,5 +44,7 @@ const requireFromFolder = function (folder) {
 }
 
 module.exports = {
-  requireFromFolder
+  requireFromFolder,
+  isWindows,
+  getHost
 }
--- packages/blue-scripts/src/server.js
+++ packages/blue-scripts/src/server.js
@@ -2,6 +2,7 @@
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
 const webpack = require('webpack')
 const paths = require('./commons/paths')
+const utils = require('./commons/utils')
 const WebpackDevServer = require('webpack-dev-server')
 const detectPort = require('./detect-port')
 const co = require('co')
@@ -11,8 +12,8 @@ module.exports = co.wrap(function * (options) {
   const config = blueConfig.get()
   const webpackConfig = config.webpack
   const port = yield detectPort(webpackConfig.devServer.port)
-  const host = webpackConfig.devServer.host
-  const serverUrl = `http://${host}:${port}`
+  const host = utils.getHost(webpackConfig.devServer.host)
+  const serverUrl = `http://${host.value}:${port}`
 
   // Add the FriendlyErrorsWebpackPlugin after everything is sorted.
   // We need this to be able to change server port in runtime and
@@ -20,7 +21,10 @@ module.exports = co.wrap(function * (options) {
   webpackConfig.plugins.push(
     new FriendlyErrorsWebpackPlugin({
       compilationSuccessInfo: {
-        messages: [`'${config.project.title}' is running on ${serverUrl}\n`]
+        messages: [
+          // see https://github.com/Blocklevel/blue-next/issues/25
+          `'${config.project.title}' is running on http://${host.display}:${port}\n`
+        ]
       }
     })
   )
