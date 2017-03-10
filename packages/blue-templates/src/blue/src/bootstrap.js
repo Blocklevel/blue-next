b45b368aace7f63885c1253ac9496f68aa15e83a
Matteo Gabriele <matteo@blocklevel.nl>
2017-03-10 13:41:29 +0100

    feat(configuration): add vue-config-manager
    closes #33

--- packages/blue-templates/src/blue/src/bootstrap.js
+++ packages/blue-templates/src/blue/src/bootstrap.js
@@ -3,10 +3,23 @@ import Vue from 'vue'
 import VueI18nManager from 'vue-i18n-manager'
 import VueRouter from 'vue-router'
 import { sync } from 'vuex-router-sync'
-
+import VueConfigManager from 'vue-config-manager'
 import store from './app/store'
 import routes from './app/routes'
 import App from './app'
+
+/**
+ * Application configuration
+ * @type {Object}
+ */
+Vue.use(VueConfigManager, {
+  defaults: {
+    debug: true,
+    api: {
+      base: 'http://localhost/api'
+    }
+  }
+})
 
 /**
  * Initialize vue-resource plugin to manage application routing
