d47c33475932c92ed5df5fdb0f7dcb5b805ac600
Matteo Gabriele <matteo@blocklevel.nl>
2017-03-13 22:53:00 +0100

    refactor(css): add same postcss plugins

--- packages/blue-scripts/src/webpack/rules/css.js
+++ packages/blue-scripts/src/webpack/rules/css.js
@@ -15,12 +15,7 @@ module.exports = {
     {
       loader: 'postcss-loader',
       options: {
-        plugins: [
-          require('postcss-advanced-variables')({ variables }),
-          require('postcss-cssnext')({
-            browsers: ['last 3 versions', 'iOS >= 8']
-          })
-        ]
+        plugins: require('../style/postcss').postcss
       }
     }
   ]
