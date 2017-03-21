1fce34f5ffea514b3ed4a54922be0632aefe439a
Matteo Gabriele <matteo@blocklevel.nl>
2017-03-21 16:21:49 +0100

    feat(bcli): add version command

--- packages/bcli/src/cli.js
+++ packages/bcli/src/cli.js
@@ -1,15 +1,18 @@
 const vorpal = require('vorpal')()
+const chalk = require('chalk')
 
 // Add all commands
 vorpal.use(require('./commands/_project'))
 vorpal.use(require('./commands/_component'))
 vorpal.use(require('./commands/_store'))
 vorpal.use(require('./commands/_share'))
+vorpal.use(require('./commands/_version'))
 
-vorpal
-  .delimiter('bcli$')
-  .show()
-  .parse(process.argv)
+// shows the current vorlap instance with delimiter
+vorpal.delimiter('bcli$').show()
+
+// process the given inputs
+vorpal.parse(process.argv)
 
 // workaround to kill node with CTRL + C
 process.on('SIGINT', () => process.exit(2))
---
+++ packages/bcli/src/commands/_version.js
@@ -0,0 +1,12 @@
+const package = require('../../package.json')
+
+module.exports = function (vorpal) {
+  const chalk = vorpal.chalk
+
+  vorpal
+    .command('version', 'check current version')
+    .alias('-v')
+    .action(function (args, callback) {
+      this.log(`you are running version ${chalk.bold(package.version)}`)
+    })
+}
