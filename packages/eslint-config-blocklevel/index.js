b0016436c0c8903c55582d20a0d973028276dd51
Matteo Gabriele <matteo@blocklevel.nl>
2017-03-21 14:46:05 +0100

    feat(eslint-config-blocklevel): add different severity based on node env value

--- packages/eslint-config-blocklevel/index.js
+++ packages/eslint-config-blocklevel/index.js
@@ -1,3 +1,14 @@
+const env = process.env.NODE_ENV
+
+/**
+ * Applies severity based on the NODE_ENV value
+ * @param  {Number} [severity=1] eslint severity index
+ * @return {Number}
+ */
+const applyDevSeverity = function (severity = 1) {
+  return env === 'development' ? 0 : severity
+}
+
 module.exports = {
   root: true,
   parser: 'babel-eslint',
@@ -22,9 +33,13 @@ module.exports = {
     node: true
   },
   rules: {
-    'no-console': 1,
+    // console.log and debugger are used in development so the level of
+    // severity is 0, but on build goes to 1 because the final product
+    // shouldn't have any console statements.
+    'no-console': applyDevSeverity(),
+    'no-debugger': applyDevSeverity(),
+
     'no-unused-vars': 1,
-    'no-debugger': 1,
     'no-use-before-define': 1,
     'max-len': [2, { 'code': 100, 'ignoreUrls': true }],
     'vue/jsx-uses-vars': 2
