ec06ed414d4bc28f178f08b484b7c8256c096072
Matteo Gabriele <matteo@blocklevel.nl>
2017-03-21 10:01:40 +0100

    sytle(blue-scripts): remove unused script in commands

--- packages/blue-scripts/bin/blue-start.js
+++ packages/blue-scripts/bin/blue-start.js
@@ -1,15 +1,5 @@
 'use strict'
-const chalk = require('chalk')
-const devServer = require('../src/server')
-const co = require('co')
 
-module.exports = co.wrap(function * () {
-  process.env.NODE_ENV = 'development'
+process.env.NODE_ENV = 'development'
 
-  return devServer().catch(err => {
-    console.log('')
-    console.error(chalk.red(err.stack))
-    console.log('')
-    process.exit(1)
-  })
-})
+module.exports = require('../src/server')
--- packages/blue-scripts/bin/blue.js
+++ packages/blue-scripts/bin/blue.js
@@ -1,14 +1,9 @@
 #!/usr/bin/env node
 'use strict'
 const cac = require('cac')
-const chalk = require('chalk')
 
 const cli = cac()
 
-cli.usage(`${chalk.yellow('blue-scripts')} [command] [options]`)
-cli.example('blue-scripts project')
-
-cli.command('*', 'Show the prompt')
 cli.command('start', 'Start dev server')
 cli.command('build', 'Build project')
 
