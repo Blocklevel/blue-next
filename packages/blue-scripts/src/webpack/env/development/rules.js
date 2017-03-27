736f25abdd6ee591f4f65d53a40cf34c97df4aec
Mick Dekkers <mickdekkersnl@gmail.com>
2017-03-27 15:19:07 +0200

    fix(blue-scripts): fix wrong babel flow transform in development

--- packages/blue-scripts/src/webpack/env/development/rules.js
+++ packages/blue-scripts/src/webpack/env/development/rules.js
@@ -1,5 +1,5 @@
 const babel = require('../../rules/babel')
 
-babel.options.plugins = [require.resolve('babel-plugin-transform-flow-strip-types')]
+babel.options.plugins = [require.resolve('babel-plugin-transform-flow-comments')]
 
 module.exports = [babel]
