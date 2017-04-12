30dce1f24a37dd449cd366f67f09b25f637d4ada
Matteo Gabriele <matteo@blocklevel.nl>
2017-04-12 14:21:44 +0200

    refactor(blue-scritps): add comments and separate configuration objects

--- packages/blue-scripts/index.js
+++ packages/blue-scripts/index.js
@@ -1,7 +1,21 @@
+/**
+ * Returns Blue configuration object
+ * @return {Object}
+ */
 function getConfig () {
-  return require('./commons/config').get().webpack
+  return require('./commons/config').get()
+}
+
+/**
+ * Returns final webpack configuration object based on the current NODE_ENV value
+ * The configuration is the merge between the current environment and bcli.config.js options 
+ * @return {Object}
+ */
+function getWebpackConfig () {
+  return getConfig().webpack
 }
 
 module.exports = {
-  getConfig
+  getConfig,
+  getWebpackConfig
 }
