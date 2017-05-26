aaef85eb000a11664be15c6a201483d92757b62d
Matteo Gabriele <matteo@blocklevel.nl>
2017-05-26 14:36:33 +0200

    refactor(blue-templates): remove logger plugin

--- packages/blue-templates/src/blue/src/core/create-store.js
+++ packages/blue-templates/src/blue/src/core/create-store.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import logger from 'vuex/dist/logger'
 
 const isDebug = process.env.NODE_ENV !== 'production'
 
@@ -8,20 +7,12 @@ Vue.use(Vuex)
 
 export default function createStore () {
   const vuexContext = require.context('../app/data/store', true, /index.js$/)
-  const plugins = []
-
-  if (isDebug) {
-    plugins.push(
-      logger()
-    )
-  }
 
   return new Vuex.Store({
     modules: vuexContext.keys().reduce((map, item) => {
       const key = item.split('/')[1]
       return { ...map, [key]: vuexContext(item).default }
     }, {}),
-    plugins,
     strict: isDebug
   })
 }
