4065209df494215341e85ac2850412b8b10774c7
Jonas Kuiler <jonas@blocklevel.nl>
2017-07-14 19:49:18 +0200

    fix(blue-scripts): make sure lint script exits with 1 when it finds errors

--- packages/blue-scripts/scripts/lint.js
+++ packages/blue-scripts/scripts/lint.js
@@ -4,5 +4,8 @@ const config = require('eslint-config-blue')
 const cli = new CLIEngine(config)
 const report = cli.executeOnFiles([`${process.cwd()}/src/**/*.js`])
 const formatter = cli.getFormatter()
+const hasErrors = (report.errorCount > 0)
 
 console.log(formatter(report.results))
+
+process.exit(hasErrors ? 1 : 0)
