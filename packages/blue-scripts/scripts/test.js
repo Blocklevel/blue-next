3ff248aea4ce0ccbf6f6654babd9a38342b0220e
Matteo Gabriele <matteo@blocklevel.nl>
2017-07-27 09:56:38 +0200

    feat(blue-scripts): add possibility to add flags on the test command

--- packages/blue-scripts/scripts/test.js
+++ packages/blue-scripts/scripts/test.js
@@ -2,11 +2,8 @@ const jest = require('jest')
 const path = require('path')
 const argv = require('minimist')(process.argv.slice(2))
 
-jest.runCLI({
-  // sometimes we need to clear the cache so we can pass a --no-cache options
-  // when we run the command
-  cache: typeof argv.cache === 'undefined' ? true : argv.cache,
+delete argv._
+
+jest.runCLI(Object.assign(argv, {
   config: require('blue-jest')
-}, [
-  process.cwd()
-])
+}), [ process.cwd() ])
