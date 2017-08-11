1ef49afef757d1be79f967883fb22ee7306137fa
Matteo Gabriele <matteo@blocklevel.nl>
2017-08-11 15:56:28 +0200

    style: copy changes

--- packages/bcli/index.js
+++ packages/bcli/index.js
@@ -1,21 +1,16 @@
 #!/usr/bin/env node
 const semver = require('semver')
 const chalk = require('chalk')
-
 const currentNodeVersion = process.versions.node
 
 if (!semver.satisfies(currentNodeVersion, '>=6.10.x')) {
-  /* eslint-disable */
   console.error(chalk.red(`
     You are running Node ${currentNodeVersion}
-
     Blue CLI requires Node 6.10 or higher.
-
     Please update your version of Node.
-  `)
-  )
+  `))
+
   process.exit(1)
-  /* eslint-enable */
 }
 
 require('./src/cli')
